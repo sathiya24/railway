914f0a7a55dca79e06c2b9d24e097f46e2f0e040
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2022-12-06 07:21:05 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8083;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+res.send('<h2 style="color: purple"> Welcome to Shelfperks Apps!!<h2>');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
