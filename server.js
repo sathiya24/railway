3a9e29bca475cb6f390bb6d80d863f1da7d9c7e9
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2022-12-09 14:54:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = 8083;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.get('/employee', (req, res) => {
+    
+    res.send('Employee Onboaring');
+});
 app.get('/', (req, res) => {
 res.send('<h2 style="color: purple"> Welcome to the NodeApp!!<h2>');
 });
