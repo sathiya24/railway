1d0e2a5bebf6577e3792caba3746bc12944bc54a
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2022-12-09 14:59:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/employee', (req, res) => {
     res.send('Employee Onboaring');
 });
 app.get('/', (req, res) => {
-res.send('<h2 style="color: purple"> Welcome to the NodeApp!!<h2>');
+res.send('<h2 style="color: purple"> Welcome to the NodeApp<h2>');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
