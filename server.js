e9ae824a54e15f90bfed57f74661cb4a33fecf42
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2022-12-10 09:09:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/employee', (req, res) => {
     
-    res.send('Employee Onboaring');
+    res.send('New Employee Onboaring');
 });
 app.get('/', (req, res) => {
-res.send('<h2 style="color: purple"> Welcome to the NodeApp<h2>');
+res.send('<h2 style="color: purple"> Welcome to the NodeApps<h2>');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
